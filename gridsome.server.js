7b64016cf918a143fb5aadf5fde9e2203aa50235
Marcus Reinhardt <webstone@gmail.com>
2020-02-10 14:04:28 +0100

    initial load

---
+++ gridsome.server.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+const imageType = require('image-type')
+const hash = crypto.createHash('sha256')
+const imageDownload = require('image-download')
+
+module.exports = function (api, options) {
+
+    api.loadSource(({
+        addSchemaTypes
+    }) => {
+        addSchemaTypes(`
+            type ${options.typeName} implements Node @infer {
+                ${options.targetField}: Image
+            }
+        `)
+    })
+
+    api.loadSource(async actions => {
+
+        var nodes = actions.getCollection(options.typeName);
+
+        nodes.data().forEach(async function (node) {
+
+            if (node[options.sourceField]) {
+                await getRemoteImage(node, nodes, options);
+            }
+        })
+    })
+}
+
+async function getRemoteImage(node, collection, options) {
+
+    await imageDownload(node[options.sourceField]).then(buffer => {
+        
+        hash.update(node[options.sourceField]);
+        const type = imageType(buffer);
+        var targetFileName = hash.digest('hex');
+        const filePath = path.resolve('./', options.targetPath, `${targetFileName}.${type.ext}`)        
+
+        fs.writeFile(filePath, buffer, (err) => console.log(err ? err : ''));
+
+        node[options.targetField] = filePath;
+
+        collection.updateNode(node);
+    });
+
+}
