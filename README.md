4423de19776d51743d31f78f1ad72f1e8f08937a
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2020-02-17 15:58:35 +0100

    Array support

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 This is a simple plugin, which is based on a discord discussion.
 It's more a workaround than a permanent solution.
 
-The plugin should work with any data source, but I have tested it only with `source-filesystem`
+The plugin should work with any data source, but I have tested it only with `source-filesystem`.
 
+## Features
+
+* Download of remote images
+* Support of multiple images ( see example )
 
 ## Install
 
@@ -27,7 +31,16 @@ module.exports = {
         'typeName' : 'Entry',
         'sourceField': 'remoteImage',
         'targetField': 'imageDownloaded',
-        'targetPath': 'src/assets/remoteImages'
+        'targetPath': './src/assets/remoteImages'
+      }
+    },
+    {
+      use: '@noxify/gridsome-plugin-image-download',
+      options: {
+        'typeName' : 'Entry',
+        'sourceField': 'remoteImages',
+        'targetField': 'imagesDownloaded',
+        'targetPath': './src/assets/remoteImages'
       }
     }
   ]
@@ -44,50 +57,107 @@ Defines the collection where the script should update the nodes
 Defines the graphql field which contains the remote image url
 
 * `targetField` 
-Defines the field name which will be generated. The field is from Type `Image`
+Defines the field name which will be generated. 
+The field is from Type `Image` or `[Images]` in case the source field is not a string.
 
 * `targetPath`
 Defines the target directory for the downloaded images
-If you set `src/assets/remoteImages`, it will save the images to `<projectroot>/src/assets/remoteImages/`
+If you set `./src/assets/remoteImages`, it will save the images to `<projectroot>/src/assets/remoteImages/`
 
 > **You have to ensure, that the defined path is valid and the directory exists.**
 > It's currently not possible to use `~` or `@`.
 
 # Example 
 
-## Markdown File
+## Markdown Files
 
 > /content/entries/entry1.md
 
 ```md
 ---
 title: First Post
-excerpt: First Post
-date: 2020-01-14T21:53:14.578Z
 remoteImage: https://images.unsplash.com/photo-1580451998921-c1e6e1ababe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80
 ---
 
-Image Credits: https://unsplash.com/@sumit_saharkar
+Image Credits: https://unsplash.com/
+```
+
+> /content/entries/entry2.md
+
+```md
+---
+title: Second Post
+excerpt: Second Post
+date: 2020-01-14T21:53:14.578Z
+remoteImages: 
+  - https://images.unsplash.com/photo-1525013066836-c6090f0ad9d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80
+  - https://images.unsplash.com/photo-1546489545-697049cfdc1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2872&q=80
+---
+
+Image Credits: https://unsplash.com/
+```
+
+## GraphQL Query
+
+```graphql
+{
+  allEntry {
+    edges {
+      node {
+        title
+        imageDownloaded
+        imagesDownloaded {
+          image
+        }
+      }
+    }
+  }
+}
+
 ```
 
 ## GraphQL Result
 
-```
+```json
 {
-    "title": "First Post",
-    "imageDownloaded": {
-        "type": "image",
-        "mimeType": "image/jpeg",
-        "src": "/assets/static/src/assets/remoteImages/a95472d2a0d9513e9146aefc4829217a94c35cca00be7ad01aa6712d80f8cbec.jpg?width=668&fit=cover&key=4bebc73",
-        "size": {
-        "width": 668,
-        "height": 1002
+  "data": {
+    "allEntry": {
+      "edges": [
+        {
+          "node": {
+            "title": "Second Post",
+            "imageDownloaded": {
+              "type": "image",
+              "mimeType": "image/jpeg",
+              //... and all other image properties
+            },
+            "imagesDownloaded": null
+          }
         },
-        "sizes": "(max-width: 668px) 100vw, 668px",
-        "srcset": [
-        "/assets/static/src/assets/remoteImages/a95472d2a0d9513e9146aefc4829217a94c35cca00be7ad01aa6712d80f8cbec.jpg?width=668&fit=cover&key=4bebc73 668w"
-        ],
-        "dataUri": "data:image/..."
+        {
+          "node": {
+            "title": "First Post",
+            "imageDownloaded": null,
+            "imagesDownloaded": [
+              {
+                "image": {
+                  "type": "image",
+                  "mimeType": "image/jpeg",
+                  //... and all other image properties
+                }
+              },
+              {
+                "image": {
+                  "type": "image",
+                  "mimeType": "image/jpeg",
+                  //... and all other image properties
+                }
+              }
+            ]
+          }
+        }
+      ]
     }
+  }
 }
 ```
--- gridsome.server.js
+++ gridsome.server.js
@@ -3,46 +3,163 @@ const path = require('path')
 const crypto = require('crypto')
 const imageType = require('image-type')
 const imageDownload = require('image-download')
+const validate = require('validate.js')
+const chalk = require('chalk')
+const _ = require('lodash')
 
-module.exports = function (api, options) {
+class ImageDownloader {
     
-    api.loadSource(({
-        addSchemaTypes
-    }) => {
-        addSchemaTypes(`
-            type ${options.typeName} implements Node @infer {
-                ${options.targetField}: Image
+    constructor(api, options) {
+
+        //no one is perfect, so we check that all required
+        //config values are defined in `gridsome.config.js`
+        const validationResult = this.validateOptions(options);
+        if( validationResult ) {
+            console.log();
+            console.log(`${chalk.yellowBright('Remote images are not downloaded. Please check your configuration.')}`)
+            console.log(`${chalk.yellowBright('* '+validationResult.join('\n* '))}`)
+            console.log();
+
+            return null;
+        }
+
+        this.options = options;
+
+        api.loadSource(async actions => {
+        
+            //check and ensure that at least one node contains
+            //the defined source field - if not, we don't have to continue
+            const fieldType = this.getFieldType(actions);
+            
+            if( !fieldType ) {
+                console.log();
+                console.log(`${chalk.yellowBright('Remote images are not downloaded.')}`)
+                console.log(`${chalk.yellowBright(`* Unable to find a node where "${this.options.sourceField}" is filled.`)}`)
+                console.log();
+
+                return null;
+            }
+
+            //update the graphql schema
+            this.addFieldToSchema(actions, fieldType)
+
+            //download the images and update the nodes
+            await this.updateNodes(actions, fieldType)            
+        });
+    }
+
+    addFieldToSchema(actions, fieldType) {
+        const schemaType = (fieldType == 'string') ? 'Image' : '[Images]';
+        
+        //it seems that `[Image]` isn't supported, yet
+        //created this type as workaround
+        actions.addSchemaTypes(`
+            type Images  {
+                image: Image
             }
         `)
-    })
+        
+        //extend the existing schema
+        const result = actions.addSchemaTypes(`
+            type ${this.options.typeName} implements Node @infer {
+                ${this.options.targetField}: ${schemaType}
+            }
+        `)        
+    }
 
-    api.loadSource(async actions => {
+    async updateNodes(actions, fieldType) {
 
-        var nodes = actions.getCollection(options.typeName);
+        var collection = actions.getCollection(this.options.typeName);
 
-        nodes.data().forEach(async function (node) {
-            if (node[options.sourceField]) {
-                await getRemoteImage(node, nodes, options);
+        var that = this;
+        collection.data().forEach(async function (node) {
+            if (node[that.options.sourceField]) {
+                const imagePaths = await that.getRemoteImage(node, fieldType);
+                if( fieldType == 'string' ) {
+                    node[that.options.targetField] = imagePaths[0];
+                } else {
+                   
+                    node[that.options.targetField] = _.map(imagePaths, function(imagePath) {
+                        return {
+                            image: imagePath
+                        };
+                    });   
+                }
+
+                collection.updateNode(node);
             }
         })
-    })
-}
 
-async function getRemoteImage(node, collection, options) {
+    }
 
-    await imageDownload(node[options.sourceField]).then(buffer => {
+    async getRemoteImage(node, fieldType) {
+
+        var that = this;
+
+        const sourceField = this.options.sourceField;
+        const imageSources = (fieldType == 'string') ? [node[sourceField]] : node[sourceField];
         
-        const hash = crypto.createHash('sha256');
-        hash.update(node[options.sourceField]);
-        const type = imageType(buffer);
-        var targetFileName = hash.digest('hex');
-        const filePath = path.resolve('./', options.targetPath, `${targetFileName}.${type.ext}`)        
+        let imagePaths = await Promise.all(
+            _.map(imageSources, async (imageSource) => {
+                return await imageDownload(imageSource).then(buffer => {
 
-        fs.writeFile(filePath, buffer, (err) => console.log(err ? err : ''));
+                    const hash = crypto.createHash('sha256');
+                    hash.update(imageSource);
+                    var targetFileName = hash.digest('hex');
+                    
+                    const type = imageType(buffer);
 
-        node[options.targetField] = filePath;
+                    const filePath = path.resolve(
+                        that.options.targetPath, 
+                        `${targetFileName}.${type.ext}`
+                    )
 
-        collection.updateNode(node);
-    });
+                    fs.writeFile(filePath, buffer, (err) => console.log(err ? err : ''));
+                    return filePath;
+                });
+            })
+        );
+        
+        return imagePaths;
+    }
 
+    getFieldType(actions) {
+
+        const nodeCollection = actions.getCollection(this.options.typeName);
+
+        let findQuery = {};
+        
+        //details about this definition can be found here
+        //https://github.com/techfort/LokiJS/wiki/Query-Examples#find-operator-examples-
+        findQuery[this.options.sourceField] = {
+            '$exists': true
+        };
+
+        const node = nodeCollection.findNode(findQuery);
+
+        return (node) ? typeof node[this.options.sourceField] : false;
+    }
+
+    validateOptions(options={}) {
+        const contraintOption = {
+            presence: {
+                allowEmpty: false
+            }
+        };
+
+        const constraints = {
+            typeName: contraintOption,
+            sourceField: contraintOption,
+            targetField: contraintOption,
+            targetPath: contraintOption
+        };
+
+        const validationResult = validate(options, constraints, {
+            format: "flat"
+        });
+
+        return validationResult;
+    }
 }
+
+module.exports = ImageDownloader
