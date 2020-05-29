5544515682c4fb0eb53bf64241d251b8ba0b52cb
Marcus Reinhardt <webstone@gmail.com>
2020-05-29 13:23:01 +0200

    Update gridsome.server.js

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,9 +6,10 @@ const imageDownload = require('image-download')
 const validate = require('validate.js')
 const chalk = require('chalk')
 const _ = require('lodash')
+const normalizeUrl = require('normalize-url')
+
 
 class ImageDownloader {
-    
     constructor(api, options) {
 
         //no one is perfect, so we check that all required
@@ -24,83 +25,133 @@ class ImageDownloader {
         }
 
         this.options = options;
+        this.api = api;
 
-        api.loadSource(async actions => {
-        
-            //check and ensure that at least one node contains
-            //the defined source field - if not, we don't have to continue
-            const fieldType = this.getFieldType(actions);
-            
-            if( !fieldType ) {
-                console.log();
-                console.log(`${chalk.yellowBright('Remote images are not downloaded.')}`)
-                console.log(`${chalk.yellowBright(`* Unable to find a node where "${this.options.sourceField}" is filled.`)}`)
-                console.log();
+        //initialize the `loadImage` event and make 
+        //it available before we run the `onBootstrap` command
+        this.initializeEvent(api);
 
-                return null;
-            }
-
-            //update the graphql schema
-            this.addFieldToSchema(actions, fieldType)
-
-            //download the images and update the nodes
-            await this.updateNodes(actions, fieldType)            
+        //create a new type `Images` which is required
+        //for array support
+        //also add a new field to the defined collection
+        //to store the downloaded images
+        api.createSchema(({ addSchemaTypes }) => {
+            const fieldType = this.getFieldType(api, options);
+            this.generateSchemaType(addSchemaTypes, fieldType);
         });
+
+        //run the plugin code, after gridsome finished all their work ( right? )
+        api.onBootstrap(() => this.loadImages())
     }
 
-    addFieldToSchema(actions, fieldType) {
+    /**
+     * Create a new event via the gridsome plugin api
+     * reference: node_modules/gridsome/lib/app/PluginAPI.js
+     */
+    initializeEvent(api) {
+        api._on('loadImage', this.runDownloader)
+    }
+
+    /**
+     * Run the defined event with the required
+     * arguments - i have no clue why `this` is not available
+     * but I'm too tired to check this in detail...
+     * Defining the needed methods is fine for me :) 
+     */
+    async loadImages() {
+        await this.run('loadImage', null, {
+            getFieldType: this.getFieldType,
+            getRemoteImage: this.getRemoteImage,
+            updateNodes: this.updateNodes,
+            options: this.options
+        })
+    }
+
+    /**
+     * Defined in `initializeEvent`
+     * Called via `loadImages`
+     */
+    async runDownloader(plugin, api) {
+        const fieldType = plugin.getFieldType(api, plugin.options);
+        await plugin.updateNodes(api, fieldType, plugin)
+    }
+
+    getFieldType(api, options) {
+        
+        const nodeCollection = api._app.store.getCollection(options.typeName);
+
+        let findQuery = {};
+
+        //details about this definition can be found here
+        //https://github.com/techfort/LokiJS/wiki/Query-Examples#find-operator-examples-
+        findQuery[options.sourceField] = {
+            '$exists': true
+        };
+
+        const node = nodeCollection.findNode(findQuery);
+
+        //we're using the lodash get functionality
+        //to allow a dot notation in the source field name
+        return (node) ? typeof _.get(node, options.sourceField) : false;
+    }
+
+    generateSchemaType(addSchemaTypes, fieldType) {
+        
         const schemaType = (fieldType == 'string') ? 'Image' : '[Images]';
         
-        //it seems that `[Image]` isn't supported, yet
-        //created this type as workaround
-        actions.addSchemaTypes(`
-            type Images  {
-                image: Image
-            }
-        `)
-        
+        addSchemaTypes(
+            `
+                type Images  {
+                    image: Image
+                }
+            `
+        );
+
         //extend the existing schema
-        const result = actions.addSchemaTypes(`
-            type ${this.options.typeName} implements Node @infer {
-                ${this.options.targetField}: ${schemaType}
-            }
-        `)        
+        addSchemaTypes(
+            `
+                type ${this.options.typeName} implements Node @infer {
+                    ${this.options.targetField}: ${schemaType}
+                }
+            `
+        );  
     }
 
-    async updateNodes(actions, fieldType) {
+    async updateNodes(api, fieldType, plugin) {
+        
+        var collection = api._app.store.getCollection(plugin.options.typeName);
 
-        var collection = actions.getCollection(this.options.typeName);
-
-        var that = this;
         collection.data().forEach(async function (node) {
-            if (node[that.options.sourceField]) {
-                const imagePaths = await that.getRemoteImage(node, fieldType);
+            
+            if (_.get(node,plugin.options.sourceField)) {
+                const imagePaths = await plugin.getRemoteImage(node, fieldType, plugin.options);
+
                 if( fieldType == 'string' ) {
-                    node[that.options.targetField] = imagePaths[0];
+                    node[plugin.options.targetField] = imagePaths[0];
                 } else {
                    
-                    node[that.options.targetField] = _.map(imagePaths, function(imagePath) {
+                    node[plugin.options.targetField] = _.map(imagePaths, function(imagePath) {
                         return {
                             image: imagePath
                         };
                     });   
                 }
 
-                collection.updateNode(node);
+                var res = collection.updateNode(node);
             }
         })
-
     }
 
-    async getRemoteImage(node, fieldType) {
+    async getRemoteImage(node, fieldType, options) {
 
-        var that = this;
-
-        const sourceField = this.options.sourceField;
-        const imageSources = (fieldType == 'string') ? [node[sourceField]] : node[sourceField];
+        const sourceField = options.sourceField;
+        const imageSources = (fieldType == 'string') ? [_.get(node,sourceField)] : _.get(node,sourceField);
         
         let imagePaths = await Promise.all(
             _.map(imageSources, async (imageSource) => {
+
+                imageSource = normalizeUrl(imageSource,{'forceHttps' : true})
+                                
                 return await imageDownload(imageSource).then(buffer => {
 
                     const hash = crypto.createHash('sha256');
@@ -108,15 +159,19 @@ class ImageDownloader {
                     var targetFileName = hash.digest('hex');
                     
                     const type = imageType(buffer);
-                    
-                    if (!fs.existsSync(that.options.targetPath)) {
-                        fs.mkdirSync(that.options.targetPath)
-                    }
 
                     const filePath = path.resolve(
-                        that.options.targetPath, 
+                        options.targetPath, 
                         `${targetFileName}.${type.ext}`
                     )
+
+                    if( fs.existsSync(filePath) ) {
+                        return filePath;
+                    }
+                    
+                    if (!fs.existsSync(options.targetPath)) {
+                        fs.mkdirSync(options.targetPath)
+                    }
 
                     fs.writeFile(filePath, buffer, (err) => console.log(err ? err : ''));
                     return filePath;
@@ -127,24 +182,35 @@ class ImageDownloader {
         return imagePaths;
     }
 
-    getFieldType(actions) {
+    /**********************
+     * Helpers
+     **********************/
 
-        const nodeCollection = actions.getCollection(this.options.typeName);
+    /**
+     * Copied from node_modules/gridsome/lib/app/Plugins.js
+     */
+    async run(eventName, cb, ...args) {
 
-        let findQuery = {};
-        
-        //details about this definition can be found here
-        //https://github.com/techfort/LokiJS/wiki/Query-Examples#find-operator-examples-
-        findQuery[this.options.sourceField] = {
-            '$exists': true
-        };
+        if (!this.api._app.plugins._listeners[eventName]) return []
 
-        const node = nodeCollection.findNode(findQuery);
+        const results = []
 
-        return (node) ? typeof node[this.options.sourceField] : false;
+        for (const entry of this.api._app.plugins._listeners[eventName]) {
+            if (entry.options.once && entry.done) continue
+
+            const { api, handler } = entry
+            const result = typeof cb === 'function'
+                ? await handler(cb(api))
+                : await handler(...args, api)
+
+            results.push(result)
+            entry.done = true
+        }
+
+        return results
     }
 
-    validateOptions(options={}) {
+    validateOptions(options = {}) {
         const contraintOption = {
             presence: {
                 allowEmpty: false
